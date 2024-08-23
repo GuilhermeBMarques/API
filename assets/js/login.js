19c748b72f481509acdeb72db0e20ffd20086aee
Guilherme <guimc0507@gmail.com>
2024-08-22 22:14:36 -0300

    .

---
+++ assets/js/login.js
@@ -0,0 +1,27 @@
+// Alterna entre os formulários de login e registro
+document.getElementById('showRegister').addEventListener('click', function() {
+    document.getElementById('loginContainer').classList.add('hidden');
+    document.getElementById('registerContainer').classList.remove('hidden');
+});
+
+document.getElementById('showLogin').addEventListener('click', function() {
+    document.getElementById('registerContainer').classList.add('hidden');
+    document.getElementById('loginContainer').classList.remove('hidden');
+});
+
+const registerEmail = document.querySelector("#registerEmail");
+const registerContainer = document.querySelector("#registerContainer");
+
+registerContainer.addEventListener("submit", function (event) {
+    const email = registerEmail.value;
+    const isValid = validateEmail(email);
+
+    if (!isValid) {
+        event.preventDefault(); 
+    }
+});
+
+const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+    return regex.test(email);
+};
